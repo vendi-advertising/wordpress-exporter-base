fbfe7f6cceab478b56a35c86ca7a3c69fa383fc9
Chris Haas <chris@vendiadvertising.com>
2019-03-06 10:24:08 -0600

    Another namespace problem

--- src/Type/Scalar/Text.php
+++ src/Type/Scalar/Text.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Vendi\Dumper\Type\Scalar;
 
+use Vendi\Dumper\Type\GenericScalar;
+
 final class Text extends GenericScalar
 {
     public function __construct($value)
