b5317508947d8a7cbbb677f7d494b690cef32339
Chris Haas <chris@vendiadvertising.com>
2019-03-06 11:23:48 -0600

    Added date as a scalar

---
+++ src/Type/Scalar/Date.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vendi\Dumper\Type\Scalar;
+
+use Vendi\Dumper\Type\GenericScalar;
+
+final class Date extends GenericScalar
+{
+    public function __construct(?\DateTime $value)
+    {
+        parent::__construct('date', $value);
+    }
+
+    public function getValue()
+    {
+        return $this->value ? $this->value->toString('Y-m-d') : null;
+    }
+
+    public static function create_from_string(?string $value) : self
+    {
+        if (!$value) {
+            return new self(null);
+        }
+
+        return new self(DateTime::createFromFormat('Y-m-d', $value));
+    }
+}
